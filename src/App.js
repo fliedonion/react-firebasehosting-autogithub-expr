f2a9cb12051f946baa6566585ccfa82633439745
Takahiro Kitahara <takahiro@mbp16-19.case-of-t.local>
2020-11-28 03:05:53 +0900

    implement todo list body

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Container, Form, Input, InputGroup, InputGroupAddon, Button} from 'reactstrap'
+import {Container, Form, Input, InputGroup, InputGroupAddon, Button, Table} from 'reactstrap'
 import {useState} from 'react';
 
 function App() {
@@ -11,9 +11,19 @@ function App() {
     addTodo(value)
   }
   const addTodo = text => {
-    const newTodos = [...todos, text]
+    const newTodos = [...todos, {text, complete: false}]
     setTodos(newTodos)
     console.log(newTodos)
+  }
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].complete = !newTodos[index].complete
+    setTodos(newTodos)
   }
 
   return (
@@ -30,6 +40,30 @@ function App() {
             </InputGroupAddon>
           </InputGroup>
         </Form>
+      </Container>
+      <Container>
+        <Table>
+          <tbody>
+            {todos && todos.map((todo, index) => (
+              <tr key={index}>
+                <th className="text-left"
+                  style={{ textDecoration: todo.complete ? 'line-through': ''}}>
+                  {todo.text}
+                </th>
+                <td className="text-right">
+                  <Button color={ todo.complete ? "secondary" : "success" }
+                    className="mr-2"
+                    onClick={() => completeTodo(index)}>
+                      {todo.complete ? '完了' : '未' }</Button>
+                  <Button 
+                    onClick={() => removeTodo(index)}
+                    color="danger">削除</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
       </Container>
     </div>
   );

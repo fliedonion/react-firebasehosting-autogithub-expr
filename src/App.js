51809ef96d4356d8cb566c644f74d474ed58aaba
Takahiro Kitahara <takahiro@mbp16-19.case-of-t.local>
2020-11-28 12:08:35 +0900

    [WIP] migrate with ContextAPI : Implement Context Provider

--- src/App.js
+++ src/App.js
@@ -5,26 +5,13 @@ import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
-  // 左辺、カーリーブレイスではない。
-  const [todos, setTodos] = useState([])
 
-  const addTodo = text => {
-    const newTodos = [...todos, {text, complete: false}]
-    setTodos(newTodos)
-  }
-  const removeTodo = index => {
-    const newTodos = [...todos]
-    newTodos.splice(index, 1)
-    setTodos(newTodos)
-  }
-  const completeTodo = index => {
-    const newTodos = [...todos]
-    newTodos[index].complete = !newTodos[index].complete
-    setTodos(newTodos)
-  }
+  // stateとfunctionはcontextに移動された。
+  const todos = []  // to avoid error
+  const addTodo = () => {} // to avoid error 
   const todolistProps = {
     // todos, completeTodo, removeTodo // stateはあまり参照の連鎖を作らない方がいいのかもと思って直接渡すことにした。
-    completeTodo, removeTodo,
+    completeTodo : () => {} , removeTodo : () => {},
   }
 
   return (
--- src/contexts/TodosContext.js
+++ src/contexts/TodosContext.js
@@ -3,9 +3,29 @@ import React, { createContext, useState } from 'react'
 export const TodosContext = createContext()
 
 const TodosContextProvider = (props) => {
+    const [todos, setTodos] = useState([])
+
+    const addTodo = text => {
+      const newTodos = [...todos, {text, complete: false}]
+      setTodos(newTodos)
+    }
+    const removeTodo = index => {
+      const newTodos = [...todos]
+      newTodos.splice(index, 1)
+      setTodos(newTodos)
+    }
+    const completeTodo = index => {
+      const newTodos = [...todos]
+      newTodos[index].complete = !newTodos[index].complete
+      setTodos(newTodos)
+    }
+  
     // accept and deploy consumer components.
+    // set state, function to value props to pass the actual consumer components.
     return (
-        <TodosContext.Provider>
+        <TodosContext.Provider
+            value={{todos, addTodo, removeTodo, completeTodo}}
+            >
             {props.children} 
         </TodosContext.Provider>
     )
